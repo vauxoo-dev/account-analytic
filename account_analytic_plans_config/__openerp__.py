97b987e823496cabcd991e55411f63bf4431c3e8
Nhomar - Vauxoo <nhomar@gmail.com>
2014-07-20 03:00:02 -0430

    [ADD] Module account_analytic_plans_config to configure this module account_analytic_plans

---
+++ account_analytic_plans_config/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Programmed by: Nhomar Hernandez <nhomar@vauxoo.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Configurre account analytic plans",
+    "version": "0.1",
+    "author": "Vauxoo",
+    "website": "www.vauxoo.com",
+    "category": "Generic Modules/Projects & Services",
+    "description": """
+    This modules is a helper module (only data) to understand how configure
+    account analytic plans.
+
+    This module is and attemp to explain throught non-update examples how such
+    module must be configured.
+
+    """,
+    "license": "AGPL-3",
+    "depends": [
+        "account_analytic_plans",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "active": False,
+    "installable": True
+}
