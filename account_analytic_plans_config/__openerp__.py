4bbfd7cdd66b09f8259d0cef9b8871d996478e3f
Nhomar - Vauxoo <nhomar@gmail.com>
2014-07-20 13:59:09 -0430

    [IMP] Added basic configuration, with this configuration you will be able to test correctly account_analytic_plans

--- account_analytic_plans_config/__openerp__.py
+++ account_analytic_plans_config/__openerp__.py
@@ -20,26 +20,55 @@
 ##############################################################################
 
 {
-    "name": "Configurre account analytic plans",
+    "name": "Configure account analytic plans",
     "version": "0.1",
     "author": "Vauxoo",
     "website": "www.vauxoo.com",
     "category": "Generic Modules/Projects & Services",
     "description": """
-    This modules is a helper module (only data) to understand how configure
-    account analytic plans.
+Pre-Config Account Analytic Plans
+=================================
 
-    This module is and attemp to explain throught non-update examples how such
-    module must be configured.
+This modules is a helper module (only data) to understand how configure
+account analytic plans.
 
+This module is and attem to explain throught non-update examples how such
+module must be configured.
+
+Other Objective of this module is to be used to make unit testing of this
+important module to ensure a secured evolution.
+
+Use Case:
+---------
+
+I have a company which need to control all the expenses related with his Human
+resources (employees) but some of those expenses needs to be paid by some
+specific customer to mantain correctly pointed the revenue of the company, the
+it needs to be reported on this way.
+
+The first expense to be controlled is buy two iMac to manage the Marketing in
+the company, the "Income" that will pay this Expense, one time
+the project finish this iMacs will be owned by the Employee with a 40% of
+discount and they will represent part of them payments, meaning that they will
+be invoiced to employees with this discount.
+
+To comply with this theretical situation, we will buy the computer and we will
+set the analytic accounting to look like this:
+
+- 50% goes to Employee A
+- 50% goes to Employee B
+- 100% goes to Project X 
     """,
     "license": "AGPL-3",
     "depends": [
+        "project",
         "account_analytic_plans",
     ],
     "data": [
+        "demo/aap_demo.xml", #One Time I finish this I will load on demo only
     ],
     "demo": [
+        #"demo/aap_demo.xml", Uncomment when develpment is ready
     ],
     "active": False,
     "installable": True
